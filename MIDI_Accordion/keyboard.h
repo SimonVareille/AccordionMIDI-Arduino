bf03b9d838331819d2c7fc0904f605e8f2351650
Simon Vareille <si.vareille@gmail.com>
2021-04-24 11:12:06 +0200

    Move classes in .cpp and .h files

---
+++ MIDI_Accordion/keyboard.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+  Accordion MIDI - Arduino
+  https://github.com/SimonVareille/AccordionMIDI-Arduino
+  Copyright � 2021 Simon Vareille
+
+  Based on projects byBrendan Vavra 2016-2017, Dimon Yegorenkov 2011 and Jason Bugeja 2014
+ *******************************************************************************
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+#ifndef __KEYBOARD_H__
+#define __KEYBOARD_H__
+
+#endif //__KEYBOARD_H__
